a98cc6ced44d76cf555c41ebf1beead7e4ed049c
JohnS300 <johnsofokleous@gmail.com>
2024-07-26 20:09:52 +0300

    Changed Readme for more clarity

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Split_json
 A script writen in Bash used to Split Json files based on a multitude of variables
+
+To run the script, set the environment variables as needed. For example:
+
+chmod +x split_json.sh
+
+INPUT_DIR="/path/to/input" OUTPUT_DIR="/path/to/output" SPLIT_FIELD="desiredField" MESSAGE_HEADER="custom_header" MESSAGE_BODY="custom_body" SENDER="custom_sender" RECEIVER="custom_receiver" OUTPUT_FILE_PREFIX="custom_prefix" ./split_json.sh
--- split_json.sh
+++ split_json.sh
@@ -41,6 +41,4 @@ done
 
 echo "All JSON files in the input directory have been processed and split based on $split_field."
 
-#To run the script, set the environment variables as needed. For example:
-#chmod +x split_json.sh
-#INPUT_DIR="/path/to/input" OUTPUT_DIR="/path/to/output" SPLIT_FIELD="desiredField" MESSAGE_HEADER="custom_header" MESSAGE_BODY="custom_body" SENDER="custom_sender" RECEIVER="custom_receiver" OUTPUT_FILE_PREFIX="custom_prefix" ./split_json.sh
+
